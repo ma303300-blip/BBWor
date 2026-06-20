c353d574c4eef20ba6b3863437f49ce020598d7b
ma303300-blip <ma303300@gmail.com>
2026-06-21 01:08:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,26 @@
-const CACHE = 'bbwoa-v1';
+const CACHE = 'bbwoa-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './bg.jpg',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  '/BBWor/',
+  '/BBWor/index.html',
+  '/BBWor/manifest.json',
+  '/BBWor/bg.jpg',
+  '/BBWor/icons/icon-192x192.png',
+  '/BBWor/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
